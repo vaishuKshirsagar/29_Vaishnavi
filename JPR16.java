781a86947bac324c9a27a04aa4480468f6aeef3d
vaishuKshirsagar <vaishnavikshirsagar1607@gmail.com>
2021-08-14 22:28:43 +0530

    Assignment 16

---
+++ JPR16.java
@@ -0,0 +1,16 @@
+// Write Program to calculate GST i.e. 18% on base price 34900/-
+package com.training;
+public class JPR16 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float GST;
+		int baseprice=34900;
+		GST = baseprice*(18.0f/100);
+		 
+		System.out.println("The value of GST on the baseprice 34900 is Rs" +GST);
+
+	}
+
+}

5de3644bb3cfc9d61210d7abd62baea055a72e8f
vaishuKshirsagar <vaishnavikshirsagar1607@gmail.com>
2021-08-14 22:26:13 +0530

    Assignment 14

---
+++ JPR14.java
@@ -0,0 +1,19 @@
+//  Write Program to Check Whether a Number is Even or Odd - 88,37,1658
+package com.training;
+
+public class JPR14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int n = 26;
+		if (n%2==0) {
+		    System.out.println("The number is even");
+		}
+		
+		else {
+			System.out.println("The number is odd");
+		}
+	}
+
+}

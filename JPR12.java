36280b76f38c08c8ec8eedbe7dfd389911d08d43
vaishuKshirsagar <vaishnavikshirsagar1607@gmail.com>
2021-08-14 22:23:05 +0530

    Assignment 12

---
+++ JPR12.java
@@ -0,0 +1,18 @@
+// Write Program to Compute Quotient and Remainder
+package com.training;
+
+public class JPR12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      
+		int a = 64;
+		int b = 7;
+		int quotient = a/b;
+		int remainder = a%b;
+	System.out.println("The value of quotient is "+quotient);
+	System.out.println("The value of remainder is "+remainder);
+		
+	}
+
+}

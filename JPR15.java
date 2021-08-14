53085f6297ec751826bb287bb99a1907c347872c
vaishuKshirsagar <vaishnavikshirsagar1607@gmail.com>
2021-08-14 22:27:24 +0530

    Assignment 15

---
+++ JPR15.java
@@ -0,0 +1,21 @@
+/* Check whether an alphabet is vowel or consonant using if..else statement
+ - a,b,e,o*/
+package com.training;
+
+public class JPR15 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		char ch='v';
+		if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+		{
+			System.out.println("Character is a vowel");
+		}
+		else
+		{
+			System.out.println("Character is a consonant");
+		}
+	}
+
+}

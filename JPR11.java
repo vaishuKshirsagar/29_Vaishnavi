5e352556c88265d0b450dd5d9a305c185eefbc01
vaishuKshirsagar <vaishnavikshirsagar1607@gmail.com>
2021-08-14 22:21:33 +0530

    Assignment 11

---
+++ JPR11.java
@@ -0,0 +1,16 @@
+// Find ASCII Value of a character 'X' and 'x'
+package com.training;
+
+public class JPR11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int X = 'X';
+		int x = 'x';
+		
+	System.out.println("The ASCII value of the 'X' is " +X);
+	System.out.println("The ASCII value of the 'x' is " +x);
+	
+	}
+
+}
